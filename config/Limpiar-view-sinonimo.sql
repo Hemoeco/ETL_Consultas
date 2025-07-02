2ad6f6c9c19f4a2c03f092a8b9dc4efb19f5051a
César A. V. Rodríguez <cesar.av.rodriguez@gmail.com>
2025-07-02 14:23:54 -0600

    Limpiar views para utilizar sinonimos

---
+++ config/Limpiar-view-sinonimo.sql
@@ -0,0 +1,46 @@
+/* ----------------------------------------------------
+-- Hemoeco Renta (2025)
+--
+-- Este script sirve para limpiar los views que se
+-- convirieron en sinonimos. Correr sólo si es necesario
+ ---------------------------------------------------- */
+
+Drop View [Comercial].[Almacen]
+Drop View [Comercial].Comprobante
+Drop View [Comercial].Concepto
+Drop View [Comercial].Documento
+Drop View [Comercial].Parametro
+Drop View [Comercial].Producto
+Drop View [Comercial].TipoCambio
+
+Drop View [Score].[Cliente]
+Drop View [Score].[ClienteSucursal]
+Drop View [Score].[ConDev]
+Drop View [Score].[ConFac]
+Drop View [Score].[ConNot]
+Drop View [Score].[ConReq]
+Drop View [Score].[ConRM]
+Drop View [Score].[CuentaBanco]
+Drop View [Score].[Deposito]
+Drop View [Score].[Devolucion]
+Drop View [Score].[EquipoNuevo]
+Drop View [Score].[EquipoRenta]
+Drop View [Score].[EquipoUsado]
+Drop View [Score].[Factura]
+Drop View [Score].[FacturaPago]
+Drop View [Score].[KardexAlta]
+Drop View [Score].[Linea]
+Drop View [Score].[LineaSucursal]
+Drop View [Score].[Modelo]
+Drop View [Score].[NotaDeCredito]
+Drop View [Score].[Obra]
+Drop View [Score].[OT]
+Drop View [Score].[OTRefaccion]
+Drop View [Score].[Pago]
+Drop View [Score].[ParaCentOper]
+Drop View [Score].[ParaPreferencias]
+Drop View [Score].[Proveedor]
+Drop View [Score].[Refaccion]
+Drop View [Score].[Requisicion]
+Drop View [Score].[RM]
+-- Drop View [Score].[rlnConFac_ConFacPers]
